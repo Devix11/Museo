7e16cf9010e8e35d8ae80c236c45f8626594bdf3
Devix11 <davideagostini05@gmail.com>
2024-03-12 09:20:57 +0100

    Update contatti.php

--- contatti.php
+++ contatti.php
@@ -3,6 +3,7 @@
 <head>
     <title>Contatti</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <header>
