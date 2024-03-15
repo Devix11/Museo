ea057f992cb9615f007d71c516c33a1f41753428
F3yor <103303362+F3yor@users.noreply.github.com>
2024-03-15 21:08:53 +0100

    correzioni grammaticali

--- index.php
+++ index.php
@@ -22,15 +22,17 @@
 
         <!-- Info museo -->
         <section id="info">
-            <h2>Benvenuti al Yellow Tulip Museum!</h2>
+            <h2>Benvenuti allo Yellow Tulip Museum!</h2>
 
-            <p>Yellow Tulip Museum è un luogo dove la storia, l'arte e la cultura si fondono per creare un'esperienza unica e coinvolgente per i visitatori di tutte le età. Situato nel cuore della città di Padova, il nostro museo celebra la ricchezza e la diversità della storia umana attraverso una vasta collezione di reperti, opere d'arte e installazioni interattive.</p>
+            <p>Lo Yellow Tulip Museum è un luogo dove la storia, l'arte e la cultura si fondono per creare un'esperienza unica e coinvolgente per i visitatori di tutte le età. Situato nel cuore della città di Padova, il nostro museo celebra la ricchezza e la diversità della storia umana attraverso una vasta collezione di reperti, opere d'arte e installazioni interattive.</p>
             <br>
-            <p>Fondato nel 2024, il Yellow Tulip Museum si impegna a preservare, studiare e condividere il patrimonio culturale del mondo. La nostra missione è educare, ispirare e stimolare la curiosità attraverso mostre innovative, programmi educativi e eventi speciali.</p>
+            <p>Fondato nel lontano 2024, lo Yellow Tulip Museum si impegna a preservare, studiare e condividere il patrimonio culturale del mondo. La nostra missione è educare, ispirare e stimolare la curiosità attraverso mostre innovative, programmi educativi ed eventi speciali.</p>
             <br>
-            <p>Dal periodo antico alla contemporaneità, esplora le innumerevoli sfaccettature della storia umana attraverso le nostre esibizioni permanenti e temporanee. Dai reperti archeologici alle opere d'arte moderne, ogni visita al Yellow Tulip Museum è un viaggio attraverso le epoche e le culture.</p>
+            <p>Dal periodo antico alla contemporaneità, esplora le innumerevoli sfaccettature della storia umana attraverso le nostre esibizioni permanenti e temporanee. Dai reperti archeologici alle opere d'arte moderne, ogni visita allo Yellow Tulip Museum è un viaggio attraverso le epoche e le culture.</p>
             <br>
-            <p>Vieni a scoprire il Yellow Tulip Museum e lasciati trasportare dalla bellezza, dall'arte e dalla storia che ci circonda. Siamo ansiosi di accoglierti e di condividere con te la nostra passione per il patrimonio culturale del mondo.</p>
+            <p>Vieni a scoprire lo Yellow Tulip Museum e lasciati trasportare dalla bellezza, dall'arte e dalla storia che ci circonda. Siamo ansiosi di accoglierti e di condividere con te la nostra passione per il patrimonio culturale del mondo.</p>
+            <br>
+            <p>E ricordate, imparare la storia non è mai stato così divertente!!</p>
         </section>
 
         <!-- Sezione Biglietti -->
@@ -46,7 +48,7 @@
                 <!-- Modulo di accesso -->
                 <div class="login">
                     <h3>Accedi</h3>
-                    <form action="login_url" method="post">
+                    <form action="" method="post">
                         <label for="username">Username:</label>
                         <input type="text" id="username" name="username" required>
                         <label for="password">Password:</label>
@@ -58,7 +60,7 @@
                 <!-- Modulo di registrazione -->
                 <div class="register">
                     <h3>Registrati</h3>
-                    <form action="register_url" method="post">
+                    <form action="" method="post">
                         <label for="new-username">Username:</label>
                         <input type="text" id="new-username" name="username" required>
                         <label for="new-password">Password:</label>
@@ -79,9 +81,13 @@
                 <div id="newsletter" style="max-width: fit-content">
                     <h3>Iscriviti alla nostra newsletter</h3>
                     <form action="subscribe.php" method="post">
-                        <input type="email" placeholder="Inserisci la tua email" name="email" required>
+                        <label>
+                            <input type="email" placeholder="Inserisci la tua email" name="email" required>
+                        </label>
                         <button type="submit">Iscriviti</button><br>
-                        <input type="checkbox" name="privacy" required> Acconsento all'informativa privacy
+                        <label>
+                            <input type="checkbox" name="privacy" required>
+                        </label> Acconsento all'informativa privacy
                     </form>
                 </div>
 
