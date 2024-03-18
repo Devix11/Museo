e9881c7e955f8ded7f7d973ba99585438bca91f5
Devix11 <davideagostini05@gmail.com>
2024-03-18 12:44:42 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
   define("DB_SERVER", "localhost");
   define("DB_USERNAME", "phpmyadmin");
   define("DB_PASSWORD", "ciaone11");
-  define("DB_NAME", "biblioteca2");
+  define("DB_NAME", "Museo");
 
   # Connessione
   $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
