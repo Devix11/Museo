5006112099680c64ba640172c801da64a45c334e
Devix11 <davideagostini05@gmail.com>
2024-03-21 13:36:00 +0100

    Update account.php

--- web/account.php
+++ web/account.php
@@ -61,6 +61,38 @@
         <section>
             <h2>Le tue prenotazioni</h2>
             <p>Qui potrai visualizzare le tue prenotazioni.</p>
+            <table>
+                <?php
+                    // Parametri di connessione al database
+                    $server = "localhost"; // Indirizzo del server MySQL (MariaDB)
+                    $user = "phpmyadmin"; // Nome utente per l'accesso al database
+                    $pwd = "ciaone11"; // Password per l'accesso al database
+                    $db = "Museo"; // Nome del database
+                    $conn = new mysqli($server, $user, $pwd, $db);
+
+                    // Controllo della connessione
+                    if ($conn->connect_error) {
+                        die("Connessione fallita: " . $conn->connect_error);
+                    }
+
+                    // Query per ottenere le prenotazioni dell'utente
+                    foreach ($conn->query('SELECT Date, E.Name, Visitors.Name, Visitors.Surname From Purchase
+                    Left join Tickets on Purchase.Ticket = Tickets.ID
+                    Left Join Exhibitions E on Tickets.Title = E.ID
+                    Left Join Visitors on Purchase.Visitor = Visitors.CF
+                    Left Join Credentials on Buyer = Credentials.CF
+                    Where Credentials.Email =' . $_SESSION["email"]) as $row) {
+                        echo "<tr>";
+                        echo "<td>" . $row["Date"] . "</td>";
+                        echo "<td>" . $row["E.Name"] . "</td>";
+                        echo "<td>" . $row["Visitors.Name"] . "</td>";
+                        echo "<td>" . $row["Visitors.Surname"] . "</td>";
+                        echo "</tr>";
+                    }
+
+                    ?>
+                    
+            </table>
         </section>
         <section>
             <h2>Le tue informazioni</h2>
