3ab76da40a4d99f264c0214bd2f8441f77856e08
angelon enrico <angelon4939@fseveri.it>
2024-03-25 09:54:08 +0100

    chore: add api for images

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,26 @@
 import fastify from 'fastify'
+import path from 'path'
 
 const server = fastify()
 
+server.register(require('fastify-static'), {
+    root: path.join(__dirname, 'folders'),
+})
+
 server.get('/ping', async (request, reply) => {
     return 'pong\n'
+})
+
+server.get('/exhibitions', async (request, reply) => {
+    const fs = require('fs').promises;
+    const folderPath = path.join(__dirname, 'exhibitions');
+    try {
+      const files = await fs.readdir(folderPath);
+      const images = files.filter((file: string) => /\.(jpg|jpeg|png|gif)$/i.test(file));
+      reply.send(images);
+    } catch (error) {
+      reply.code(500).send({ error: 'Internal Server Error' });
+    }
 })
 
 server.get('/test', async (request, reply) => {
