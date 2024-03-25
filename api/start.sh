3b7a7b33a8939178025119ef2e3d60f27e68ca7c
angelon enrico <angelon4939@fseveri.it>
2024-03-25 08:41:50 +0100

    chore(api): add start script

---
+++ api/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Nome dello screen
+SCREEN_NAME="npm_screen"
+
+# Controlla se lo screen esiste già
+if screen -list | grep -q "$SCREEN_NAME"; then
+    echo "Terminazione dello screen $SCREEN_NAME esistente..."
+    screen -S $SCREEN_NAME -X quit
+fi
+
+# Crea uno screen con il nome specificato
+screen -dmS $SCREEN_NAME
+
+# Esegue i comandi all'interno dello screen
+screen -S $SCREEN_NAME -X stuff "npm i && npm run build && npm run start$(printf \\r)"
+
+echo "Comandi eseguiti nello screen $SCREEN_NAME."
