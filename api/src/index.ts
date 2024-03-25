c9eeb97371f670553a58723d9dc439496b428187
angelon enrico <angelon4939@fseveri.it>
2024-03-25 09:04:19 +0100

    meta: open to the internet for test

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,13 +3,14 @@ import fastify from 'fastify'
 const server = fastify()
 
 server.get('/ping', async (request, reply) => {
-return 'pong\n'
+    return 'pong\n'
 })
 
-server.listen({ port: 3311 }, (err, address) => {
+// IN PRODUCTION CHANGE TO 127.0.0.1
+server.listen({ host: "0.0.0.0", port: 3311 }, (err, address) => {
 if (err) {
-console.error(err)
-process.exit(1)
+    console.error(err)
+    process.exit(1)
 }
 console.log(`Server listening at ${address}`)
 })
