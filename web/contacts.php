6a13c238cf408d11054225efe2e2f73f4368f545
Sorgattino <sorgatochristian@gmail.com>
2024-03-26 08:57:40 +0100

    Contacts.php's CSS debugging

--- web/contacts.php
+++ web/contacts.php
@@ -18,8 +18,7 @@
                 </ul>
             </nav>
         </header>
-        <div class="container">
-            <section class="info2">
+        <div class="info2">
                 <!-- Form per il contatto -->
                 <div class="col1">
                     <h2>Contattaci!</h2>
@@ -46,7 +45,6 @@
                     <p>Telefono: +39 049 827 3939</p>
                     <p>Email: info@yellowtulipmuseum.it</p>
                 </div>
-            </section>
         </div>
 
         <!-- Footer -->
