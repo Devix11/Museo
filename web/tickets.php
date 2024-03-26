d8b8d15023b40ad9585aa88a21a3a71b2473359c
Sorgattino <sorgatochristian@gmail.com>
2024-03-26 09:10:31 +0100

    Modified Tickets.php's CSS

--- web/tickets.php
+++ web/tickets.php
@@ -45,7 +45,7 @@
                     <button>COMPRA ORA</button>
                     </div>
                 </section>
-                <section class="info-content">
+                <div class="info-content">
                 <?php
                     $sql = "SELECT E.Name, E.Image, T.ValidityDate, T.ExpiringDate, P.Price FROM Exhibitions E INNER JOIN Prices P ON E.ID = P.Exhibition INNER JOIN Tickets T ON E.ID = T.Title";
             $result = $conn->query($sql);
@@ -78,7 +78,7 @@
                     </div>
                     
                 <?php endif; ?>
-                </section>
+                </div>
             </div>
             <?php
             $conn->close();
