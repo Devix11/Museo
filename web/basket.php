e0d39cb1e221ff612d84ee685df2f279e815c174
Devix11 <davideagostini05@gmail.com>
2024-03-26 09:30:36 +0100

    Update basket.php

--- web/basket.php
+++ web/basket.php
@@ -5,36 +5,37 @@
     <link rel="stylesheet" href="style.css"> <!-- Inclusione del foglio di stile CSS -->
 </head>
 <body>
-<header>
+    <header>
         <h1>Carrello</h1>
     </header>
     <div id="cart">
+        <section>
+            <h2>Carrello</h2>
+            <?php
+            session_start(); // Inizia la sessione
 
-    <section>
-        <h2>Carrello</h2>
-        <?php
-        // Inizia la sessione
-        session_start();
-
-        // Controlla se il carrello è vuoto
-        if(empty($_SESSION['cart'])) {
-            echo "Il tuo carrello è vuoto.";
-        } else {
-            // Mostra gli articoli nel carrello
-            foreach($_SESSION['cart'] as $item) {
-                echo "Prodotto: " . $item['name'] . "<br>";
-                echo "Prezzo: $" . $item['price'] . "<br>";
-                echo "<br>";
+            if(empty($_SESSION['cart'])) {
+                echo "Il tuo carrello è vuoto.";
+                $cartIsEmpty = true;
+            } else {
+                // Mostra gli articoli nel carrello
+                $cartIsEmpty = false;
+                foreach($_SESSION['cart'] as $item) {
+                    echo "Prodotto: " . $item['name'] . "<br>";
+                    echo "Prezzo: $" . $item['price'] . "<br>";
+                    echo "<br>";
+                }
             }
-        }
-
-        ?>
-
-        <button><a href="checkout.php">Procedi al Checkout<</a></button>
+            ?>
+            <button><a href="exposures.php">Continua lo shopping</a></button>
+            <?php if (!$cartIsEmpty): ?>
+                <button><a href="checkout.php">Procedi al Checkout</a></button>
+            <?php else: ?>
+                <button disabled>Procedi al Checkout</button>
+            <?php endif; ?>
         </section>
     </div>
 
-    <!-- Includi il footer -->
     <?php include('footer.php'); ?>
 </body>
 </html>
