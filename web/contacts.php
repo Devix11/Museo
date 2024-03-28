9f2a665c15a118152fb150a155005dc738246858
Sorgattino <sorgatochristian@gmail.com>
2024-03-28 10:16:02 +0100

    Modified css in contatcs.php

--- web/contacts.php
+++ web/contacts.php
@@ -43,9 +43,9 @@
                     <h2>Informazioni di Contatto</h2>
                     <p>Indirizzo: Via S. Francesco, 94, 35121 Padova PD, Italia</p>
                     <p>Telefono: +39 049 827 3939</p>
-                    <p>Email: info@yellowtulipmuseum.it</p>
+                    <p>Email: info@yellowtulipmuseum.it</p><br>
                     <p id="map" style="max-width: fit-content">
-                        <h3>Dove ci puoi trovare</h3>
+                        <h2>Dove ci puoi trovare</h2>
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2801.206506390473!2d11.877898976101477!3d45.405175171072976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477eda56d5f48cfb%3A0xbe00f67ac99d3984!2sVia%20S.%20Francesco%2C%2094%2C%2035121%20Padova%20PD!5e0!3m2!1sit!2sit!4v1710521708059!5m2!1sit!2sit" width="450" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </p>
                 </div>
